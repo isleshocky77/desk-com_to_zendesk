cf0ff04b16b22ac8162ac6cc5166475a9479ed1c
Stephen Ostrow <stephen@ostrow.tech>
2020-01-08 10:02:39 -0500

    Adding a command to list topics

---
+++ src/isleshocky77/DeskCom/Command/Topics.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace isleshocky77\DeskCom\Command;
+
+use isleshocky77\DeskCom\Api\DeskComClient;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class Topics extends Command
+{
+    protected static $defaultName = 'desk-com:topics:list';
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $client = DeskComClient::getInstance();
+
+        $uri = 'topics';
+        do {
+            $response = $client->get($uri, ['auth' => 'oauth']);
+            $payload = json_decode((string) $response->getBody());
+
+            $topics = $payload->_embedded->entries;
+
+            foreach ($topics as $topic) {
+                $output->writeln(sprintf("%s : %s", $topic->id, $topic->name));
+            }
+        } while (($uri = $payload->_links->next->href) !== null);
+
+        return 0;
+    }
+}
